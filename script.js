dc82ffd6fb561702e69b4a4daea4635f990177c3
Kalie <kaliecalnan@gmail.com>
2025-08-15 22:57:01 -0400

    Revert "Add feature to make each square random color"
    This reverts commit d35e5400647807a24b314c7d32aa0b33e77ce1c5.

--- script.js
+++ script.js
@@ -50,13 +50,5 @@ function makeGrid(size) {
 }
 
 function colorSquare(e) {
-    e.target.style.backgroundColor = `rgb(${getRandomColorValue()}, ${getRandomColorValue()}, ${getRandomColorValue()})`;
-}
-
-function getRandomColorValue() {
-    // Min and max RGB values
-    let min = 0;
-    let max = 255;
-
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    e.target.style.backgroundColor = 'black';
 }
