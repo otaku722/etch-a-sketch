92573ae5f882f90173f1f61a83fb77b9cc56aa70
Kalie <kaliecalnan@gmail.com>
2025-08-15 20:49:39 -0400

    Add pseudocode for program logic in JavaScript
    Add basic logic for program in JavaScript file. Link JavaScript file to
    HTML.

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+
+1. Create default 16x16 grid upon loading page
+2. Change background color of page if mouse is hovering over spot on page
+3. If user clicks button at top of screen, prompt user for new grid size *how many squares on each side*
+
+*** THINGS TO REMEMBER ***
+- Give each square in the grid a class to allow CSS styling
+-Set user input limit to 100 squres per side
+- New grid should take up same amount of space as old grid, with the only difference being in square density
+
+*/
